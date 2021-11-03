9e22d4aa2704d8998df0cd60dd322ea19e902841
Sun <nightfallvs0923@gmail.com>
2021-11-03 11:01:30 +0800

    [Tasks] move to pages/task-tracker

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AboutComponent } from './components/about/about.component';
-import { TasksComponent } from './components/tasks/tasks.component';
 
 const routes: Routes = [
-  {
-    path: '',
-    component: TasksComponent,
-  },
   { path: 'about', component: AboutComponent },
+  {
+    path: 'task-tracker',
+    loadChildren: () =>
+      import('./pages/task-tracker/task-tracker.module').then(
+        (mod) => mod.TaskTrackerModule
+      ),
+  },
 ];
 
 @NgModule({
---
+++ src/app/pages/task-tracker/task-tracker.module.ts
@@ -0,0 +1,34 @@
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { TasksComponent } from '../../components/tasks/tasks.component';
+import { RouterModule } from '@angular/router';
+import { HeaderComponent } from '../../components/header/header.component';
+import { ButtonComponent } from '../../components/button/button.component';
+import { AddTaskComponent } from '../../components/add-task/add-task.component';
+import { FooterComponent } from '../../components/footer/footer.component';
+import { TaskItemComponent } from '../../components/task-item/task-item.component';
+import { TaskTrackerComponent } from './task-tracker/task-tracker.component';
+
+@NgModule({
+  declarations: [
+    TasksComponent,
+    HeaderComponent,
+    ButtonComponent,
+    TaskItemComponent,
+    AddTaskComponent,
+    FooterComponent,
+    TaskTrackerComponent,
+  ],
+  imports: [
+    CommonModule,
+    RouterModule.forChild([{ path: '', component: TaskTrackerComponent }]),
+    FontAwesomeModule,
+    HttpClientModule,
+    FormsModule,
+  ],
+})
+export class TaskTrackerModule {}
